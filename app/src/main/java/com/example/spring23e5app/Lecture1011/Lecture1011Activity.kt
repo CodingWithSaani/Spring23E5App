29d9bef4efa85c7f203333ac0d23470ddc5332ee
codingwithsaani <codingwithsaani@gmail.com>
2023-04-11 11:40:11 +0500

    Lecture 10

---
+++ app/src/main/java/com/example/spring23e5app/Lecture1011/Lecture1011Activity.kt
@@ -0,0 +1,12 @@
+package com.example.spring23e5app.Lecture1011
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.spring23e5app.R
+
+class Lecture1011Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lecture1011)
+    }
+}
--- app/src/main/java/com/example/spring23e5app/MainActivity.kt
+++ app/src/main/java/com/example/spring23e5app/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.example.spring23e5app.Lecture1011.Lecture1011Activity
 import com.example.spring23e5app.Lecture6.Lecture6Activity
 import com.example.spring23e5app.Lecture89.Lecture89Activity
 
@@ -20,6 +21,11 @@ class MainActivity : AppCompatActivity()
 
         findViewById<Button>(R.id.move_l89_btn).setOnClickListener {
             var intent = Intent(MainActivity@this,Lecture89Activity::class.java)
+            startActivity(intent)
+        }
+
+        findViewById<Button>(R.id.move_l1011_btn).setOnClickListener {
+            var intent = Intent(MainActivity@this,Lecture1011Activity::class.java)
             startActivity(intent)
         }
 
